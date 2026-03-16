0caa7b5506f7c66db406bbe6eac614e5be4adac8
sachin singh <sachinkaintura4@gmail.com>
2026-03-16 23:38:27 +0530

    jdfkj

--- App.jsx
+++ App.jsx
@@ -23,6 +23,7 @@ function App(){
           <Route path="/" element={<Home/>}/>
           <Route path="login" element={<Login/>}/>
           <Route path="signup" element={<Signup/>}/>
+          <Route path=""/>
        </Routes>
    
    </BrowserRouter>
