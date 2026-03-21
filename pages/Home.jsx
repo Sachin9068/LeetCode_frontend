bfda854c9fd6417f9a44fed2d444d3bdc039d70b
sachin singh <sachinkaintura4@gmail.com>
2026-03-21 12:20:41 +0530

    create home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -146,4 +146,13 @@ return (
     </div>
   );
 }
+
+const getDifficultyBadgeColor = (difficulty)=>{
+  switch (difficulty.toLowerCase()){
+      case 'easy': return 'badge-success';
+    case 'medium': return 'badge-warning';
+    case 'hard': return 'badge-error';
+    default: return 'badge-neutral'; 
+  }
+}
 export default Home
